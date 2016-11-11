e08cead9f39bb584c2b19008d4f6052e9eb5738e
Shinan Liu <land.liu0123@gmail.com>
2016-11-11 09:57:13 -0500

    adding new javascript

--- JavaScript/mainScript.js
+++ JavaScript/mainScript.js
@@ -1,27 +1,20 @@
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-    }
-    function validateForm(){
+function validateForm(){
   var Username = document.forms["myForm"]['Username'].value;
-  if (Username==null || Username== "") {
-    alert ("first name is required");
+  if (Username==null || Username== " ") {
+    alert (" username is required");
     return false;
   }
   if (Username.length <9 ) {
-    alert ("first name have to be more then 9 character");
+    alert (" username have to be more then 9 character");
     return false;
   }
    var Password = document.forms["myForm"]['Password'].value;
-  if (Password==null || Password== "") {
-    alert ("first name is required");
+  if (Password==null || Password== " ") {
+    alert ( "Password is required");
     return false;
   }
   if (Password.length <9 ) {
-    alert ("first name have to be more then 9 character");
+    alert ("Password have to be more then 9 character");
     return false;
   }
 }
